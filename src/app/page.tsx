491dfb5fe66fd25611a0afcd51d4fa27a9da53ca
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2023-12-07 23:32:57 +0530

    next dev --turbo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { fetchDevfolio, fetchDevpost, fetchUnstop } from "@/actions/getHackathons";
+
 export default async function Home() {
   const res = await fetchUnstop();
   console.log(res);
