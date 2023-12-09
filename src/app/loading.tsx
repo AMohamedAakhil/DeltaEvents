e6634575117143cef8d1d934b51d49e3809cb7f4
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2023-12-09 13:09:07 +0530

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,8 +5,8 @@ const Loading = () => {
   return (
     <>
         <div className="p-5">
-            <Skeleton className="mt-5 w-[20rem] h-[3.5rem]" />
-            <div className="mt-10 flex flex-col md:grid md:grid-cols-2 md:gap-4 lg:grid lg:grid-cols-3 lg:gap-4">
+            <Skeleton className="w-[20rem] h-[3.5rem]" />
+            <div className="mt-5 flex flex-col md:grid md:grid-cols-2 md:gap-4 lg:grid lg:grid-cols-3 lg:gap-4">
               <Skeleton className="h-[10rem] w-full" />
               <Skeleton className="h-[10rem] w-full" />
               <Skeleton className="h-[10rem] w-full" />
