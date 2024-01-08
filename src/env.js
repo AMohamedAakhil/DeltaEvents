4e37e53e62bd5c400f042f0acaf1715fc59573f0
A Mohamed Aakhil <a.aakhilmohamed@gmail.com>
2024-01-08 22:57:39 +0530

    fix: env.js file

--- src/env.js
+++ src/env.js
@@ -7,7 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    TURSO_DATABASE_URL: z
+    DATABASE_URL: z
       .string()
       .url()
       .refine(
@@ -33,7 +33,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
